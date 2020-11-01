04435c5ffb6bcb341ea27b8469b38333676c790f
AdamKlejda <a.antoni@wp.pl>
2020-11-01 18:16:39 +0100

    working simple heuristics

--- simple_heur.cpp
+++ simple_heur.cpp
@@ -3,14 +3,51 @@
 #include "util.h"
 
 int simple_heur(std::string filepath, int nOfCities){
-    const int nOfNeighbours = (nOfCities * (nOfCities - 1)) / 2;
-    int *currentPermutation = RandomPermutation(nOfCities);
-    std::cout<<*currentPermutation;
-    return 1 ;
+    Matrix distanceMatrix = LoadData(filepath, nOfCities);
+
+    int *finallPath = RandomPermutation(nOfCities);
+    int icities = 1;
+    int distanceValue = 0;
+    int currentCity = finallPath[0];
+    int destinationCity = finallPath[1];
+    int currentDistance = distanceMatrix.GetValue(currentCity,destinationCity);
+    int possibleDistance = 0;
+
+    while (icities < nOfCities){
+        for( int i = icities; i<nOfCities;i++){ // sprawdzamy odległość do najbliższego miasta
+            possibleDistance = distanceMatrix.GetValue(currentCity,finallPath[i]);
+            if( currentDistance > possibleDistance){
+                currentDistance = possibleDistance;
+                destinationCity = i;
+
+            }   
+            // std::cout<<"for"<<i<<std::endl;   
+        }
+        // std::cout<<"distance: "<<currentDistance<<std::endl;    
+
+        distanceValue =distanceValue+ currentDistance;
+        std::swap(finallPath[icities],finallPath[destinationCity]);
+        icities ++;
+        currentCity = destinationCity;
+        destinationCity = finallPath[icities];
+        currentDistance = distanceMatrix.GetValue(currentCity,destinationCity);
+        // std::cout<<"while"<<icities<<std::endl;         
+
+    }
+    
+    // for(int i=1;i<nOfCities;i++){
+    //     std::cout<<finallPath[i]<<std::endl;
+    //     std::cout<<"DISTANCE "<<distanceMatrix.GetValue(i,i-1)<<std::endl;
+        
+    // }
+    return distanceValue ;
 
 }
 
 int main(){
-    int i = simple_heur("data/unpacked_ready/br17.atsp",17);
+    // int i = simple_heur("data/unpacked_ready/br17.atsp",17);
+    // std::cout<<i<<std::endl;
+    MeasureTimeOfFunctionInMilliSeconds(1, simple_heur, "data/unpacked_ready/br17.atsp",17);
     return 0;
+
 }
